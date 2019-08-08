dd979f6f801aadf1c483e7f037cb79fb322523c6
Franklin Antony <franklinanton.007@gmail.com>
2019-08-09 03:46:20 +0530

    updated api endpoints 🚧

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ connectDB();
 
 //helps to accept JSON data to API
 app.use(express.json({ extented: false }));
+
+//Defining Routes
+app.use('/', require("./routes/index"));
+app.use('/api/url', require("./routes/url"));
+
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log('Message : Server running on ${PORT}'));
